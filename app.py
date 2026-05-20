154b0e1b325b29d8d3e302c2597ba7739d7b48e5
acardenas2207 <45862810@mail.isil.pe>
2026-05-20 17:54:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -91,12 +91,12 @@ HeartRate = st.number_input("HeartRate", min_value=40, max_value=200, value=75)
 # ==========================================
 
 input_data = pd.DataFrame({
-    'Edad': [Age],
-    'Presión Sistólica': [SystolicBP],
-    'Presión Sistólica': [DiastolicBP],
-    'Nivel de glucosa': [BS],
-    'Temperatura': [BodyTemp],
-    'Freuencia Cardiaca': [HeartRate]
+    'Age': [Edad],
+    'SystolicBP': [Presión Sistólica],
+    'DiastolicBP': [Presión Sistólica],
+    'BS': [Nivel de glucosa],
+    'BodyTemp': [Temperatura],
+    'HeartRate': [Freuencia Cardiaca]
 })
 
 # ==========================================
